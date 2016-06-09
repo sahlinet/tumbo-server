f5704a12427c026c2e554c48445ef16a5f765799
Philip Sahli <philip@sahli.net>
2016-06-09 01:49:42 -0400

    Bump version: 0.1.8 → 0.1.9

--- cli/tumbo.py
+++ cli/tumbo.py
@@ -377,7 +377,7 @@ def do_ngrok():
 
 if __name__ == '__main__':
     # arguments = docopt(__doc__, version=version)
-    arguments = docopt(__doc__, version="0.1.8")
+    arguments = docopt(__doc__, version="0.1.9")
     #import pprint; pprint.pprint(arguments)
     if arguments['--ngrok-hostname'] and arguments['docker']:
 	    try:
