9f2eca71a915a1c57359cb2d5119560e5fb102e1
Philip Sahli <philip@sahli.net>
2016-09-21 15:27:06 -0400

    cleanup data_for_user function

--- tumbo/core/plugins/templatetags/datastore.py
+++ tumbo/core/plugins/templatetags/datastore.py
@@ -5,11 +5,4 @@ register = template.Library()
 @register.assignment_tag(takes_context=True)
 def data_for_user(context):
     datastore = context['datastore']
-    for data in datastore.all():
-        print data.data['testid']
-        try:
-            print data.data['user_id']
-        except:
-            pass
-    print "ID: "+str(context['user'].authprofile.internalid)
     return datastore.filter("user_id", str(context['user'].authprofile.internalid), read=True)
