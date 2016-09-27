6330ff56fbc8b8fd29e17c51fe884f4dafada2da
Philip Sahli <philip@sahli.net>
2016-09-27 01:38:17 -0400

    return None in data_for_user if user is anonymous

--- tumbo/core/plugins/templatetags/datastore.py
+++ tumbo/core/plugins/templatetags/datastore.py
@@ -5,4 +5,6 @@ register = template.Library()
 @register.assignment_tag(takes_context=True)
 def data_for_user(context):
     datastore = context['datastore']
+    if context['user'].is_anonymous():
+        return None
     return datastore.filter("user_id", str(context['user'].authprofile.internalid), read=True)
