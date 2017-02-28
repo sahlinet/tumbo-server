14bb31b4d161233dacd1ee6ea190b1f523e9f843
Philip Sahli <philip@sahli.net>
2017-02-28 09:24:54 +0000

    use sqlite db on CI

--- tumbo/tumbo/settings.py
+++ tumbo/tumbo/settings.py
@@ -68,16 +68,25 @@ WSGI_APPLICATION = 'tumbo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "tumbodev",
-        'HOST': "localhost",
-        'PORT': 16432,
-        'USER': "tumbodev",
-        'PASSWORD': "tumbodev123"
-    }
-}
+if os.environ.get('CI', None):
+	DATABASES = {
+	    'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+	    }
+	}
+
+else:
+	DATABASES = {
+	    'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': "tumbodev",
+		'HOST': "localhost",
+		'PORT': 16432,
+		'USER': "tumbodev",
+		'PASSWORD': "tumbodev123"
+	    }
+	}
 
 # If tumbo is run from an egg, use db in $HOME/.tumbo
 if "site-packages" in BASE_DIR:
