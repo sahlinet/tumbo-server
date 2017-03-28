0913b8f579920f918d49e139ca46a8dd20f76ba8
Philip Sahli <philip@sahli.net>
2017-03-28 19:18:30 +0000

    Bump version: 0.4.3 → 0.4.4

--- cli/tumbo-cli.py
+++ cli/tumbo-cli.py
@@ -514,7 +514,7 @@ def tolocaltime(dt):
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version="0.4.3")
+    arguments = docopt(__doc__, version="0.4.4")
     if arguments['--ngrok-hostname'] and arguments['docker']:
         try:
             ngrok = sh.Command("ngrok")
