6f8dd6c2be4af69dfac4fb92f7dca08ee8ca13bf
Philip Sahli <philip@sahli.net>
2018-03-21 15:49:25 +0100

    Bump version: 0.4.8-dev → 0.4.9-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-__VERSION__ = "0.4.8-dev"
+__VERSION__ = "0.4.9-dev"
 
 
 # http://stackoverflow.com/questions/14399534/how-can-i-reference-requirements-txt-for-the-install-requires-kwarg-in-setuptool
