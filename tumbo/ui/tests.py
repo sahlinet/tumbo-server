9badf60c90a95b720d499d0ac35ed043e80e28cc
Philip Sahli <philip@sahli.net>
2018-04-03 09:45:57 +0200

    add headless option to ChromeDriver

--- tumbo/ui/tests.py
+++ tumbo/ui/tests.py
@@ -9,7 +9,9 @@ User = get_user_model()
 class AccountTestCase(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.selenium = webdriver.Chrome(chrome_options=options)
         super(AccountTestCase, self).setUp()
 
         self.admin_pw = 'mypassword'
