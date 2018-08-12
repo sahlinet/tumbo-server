6f07dbbdad036247aa885df6a05d2226c8eb6af5
user name <user@domain.com>
2018-08-12 14:12:32 +0200

    Bump version: 0.5.25-dev → 0.5.26-dev

--- tumbo/core/__init__.py
+++ tumbo/core/__init__.py
@@ -4,7 +4,7 @@
     - Signals
 """
 
-__VERSION__ = "0.5.25-dev"
+__VERSION__ = "0.5.26-dev"
 
 
 from django.core.exceptions import ImproperlyConfigured
--- tumbo/core/executors/worker_engines/kube.py
+++ tumbo/core/executors/worker_engines/kube.py
@@ -154,7 +154,7 @@ class KubernetesExecutor(BaseExecutor):
                         "containers": [
                             {
                                 "env": worker_env,
-                                "image": "philipsahli/tumbo-worker:v0.5.25-dev",
+                                "image": "philipsahli/tumbo-worker:v0.5.26-dev",
                                 "imagePullPolicy": "Always",
                                 "name": self.name,
                                 "command": self._start_command,
