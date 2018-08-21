1f5b94c76749ee380e3515031e4922af43cce6be
user name <user@domain.com>
2018-08-21 06:48:32 +0200

    updatescript

--- update.sh
+++ update.sh
@@ -8,8 +8,11 @@ for pod in $(echo $pods); do
   kubectl exec -it ${pod} --namespace=tumbo --context=kubernetes-admin@kubernetes -- bash -c "pkill -HUP -f /home/tumbo/.virtualenvs/tumbo/bin/python"
 done
 
-pod=`kubectl get pods --namespace=tumbo --context=kubernetes-admin@kubernetes|grep ^background-|awk '{print $1}'`
-for i in $(git status -s|grep tumbo|awk '{print $2}'); do
-  kubectl cp $i ${pod}:./.virtualenvs/tumbo/lib/python2.7/site-packages/$i  --context=kubernetes-admin@kubernetes --namespace=tumbo 
+pods=`kubectl get pods --namespace=tumbo --context=kubernetes-admin@kubernetes|grep ^background-|awk '{print $1}'`
+for pod in $(echo $pods); do 
+  echo $pod
+  for i in $(git status -s|grep tumbo|awk '{print $2}'); do
+    kubectl cp $i ${pod}:./.virtualenvs/tumbo/lib/python2.7/site-packages/$i  --context=kubernetes-admin@kubernetes --namespace=tumbo 
+  done
+  kubectl exec -it ${pod} --namespace=tumbo --context=kubernetes-admin@kubernetes -- bash -c "pkill -HUP -f /home/tumbo/.virtualenvs/tumbo/bin/python"
 done
-kubectl exec -it ${pod} --namespace=tumbo --context=kubernetes-admin@kubernetes -- bash -c "pkill -HUP -f /home/tumbo/.virtualenvs/tumbo/bin/python"
