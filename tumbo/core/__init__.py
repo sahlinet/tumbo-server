559daeac8e99adad88d6d0a29888e0f35f967a9c
user name <user@domain.com>
2018-08-23 07:29:51 +0200

    Bump version: 0.5.33-dev → 0.5.34-dev

--- tumbo/core/__init__.py
+++ tumbo/core/__init__.py
@@ -4,7 +4,7 @@
     - Signals
 """
 
-__VERSION__ = "0.5.33-dev"
+__VERSION__ = "0.5.34-dev"
 
 
 from django.core.exceptions import ImproperlyConfigured
--- tumbo/tumbo/__init__.py
+++ tumbo/tumbo/__init__.py
@@ -1,4 +1,4 @@
 """Tumbo django project
 """
 
-__VERSION__ = '0.5.33-dev'
+__VERSION__ = '0.5.34-dev'
